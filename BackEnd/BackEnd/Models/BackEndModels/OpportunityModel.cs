78071d67ea1d0cba47ae5f142f277c2310bf2ee8
Leonardo Silva <leonardosilva.00009@gmail.com>
2024-10-25 18:42:24 +0100

    Update do OpportunityModel e dos Enums

--- BackEnd/BackEnd/Models/BackEndModels/OpportunityModel.cs
+++ BackEnd/BackEnd/Models/BackEndModels/OpportunityModel.cs
@@ -1,6 +1,46 @@
-﻿namespace BackEnd.Models.BackEndModels
+﻿using BackEnd.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Models.BackEndModels
 {
     public class OpportunityModel
     {
+
+        [Key]
+        public int OpportunityId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(2,6)]
+        public float Price { get; set; }
+
+        [Required]
+        public int Vacancies { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public Category Category {  get; set; }
+
+        [Required]
+        public Location Location { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        public float Score { get; set; }
+
+        [Required]
+        public bool IsImpulsed { get; set; }
+
     }
 }
