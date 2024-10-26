299eeb99b434492bb6df63d6240230c60e1b99de
Rafael Silva <oreosmilesoo@gmail.com>
2024-10-26 19:12:12 +0100

    Starting Opp

---
+++ BackEnd/BackEnd/Tests/Models/OpportunityModelTest.cs
@@ -0,0 +1,26 @@
+﻿using BackEnd.Controllers.Data;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace BackEnd.Tests.Models
+{
+    [TestFixture]
+    public class OpportunityModelTest
+    {
+
+        private ApplicationDbContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+        }
+
+
+
+    }
+}
