d1927a1e5d868dc298a048d195f6e56b503b25d7
fbarbosa2 <f_barbosa@outlook.pt>
2024-10-27 20:45:46 +0000

    Creating Mapper for the Opportunity Dto

--- BackEnd/BackEnd/Controllers/OpportunityController.cs
+++ BackEnd/BackEnd/Controllers/OpportunityController.cs
@@ -1,6 +1,30 @@
-﻿namespace BackEnd.Controllers
+﻿using BackEnd.Controllers.Data;
+using BackEnd.Models.BackEndModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BackEnd.Controllers
 {
-    public class OpportunityController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpportunityController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public OpportunityController(ApplicationDbContext opportunityContext) => this._context = opportunityContext;
+
+        //GET Opportunity/
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OpportunityModel>>> GetAllOpportunities()
+        {
+
+        }
+
+        //GET Opportunity/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OpportunityModel>> GetOpportunityById(int id)
+        {
+
+        }
     }
 }
---
+++ BackEnd/BackEnd/Models/Mappers/OpportunityMapper.cs
@@ -0,0 +1,29 @@
+﻿using BackEnd.Models.BackEndModels;
+using BackEnd.Models.FrontEndModels;
+
+namespace BackEnd.Models.Mappers
+{
+    public class OpportunityMapper
+    {
+        // Method to map OpportunityModel to Opportunity
+        public static Opportunity MapToDto(OpportunityModel opportunityModel)
+        {
+            if (opportunityModel == null)
+                return null;
+
+            return new Opportunity
+            {
+                opportunityId = opportunityModel.OpportunityId,
+                name = opportunityModel.Name,
+                price = opportunityModel.Price,
+                vacancies = opportunityModel.Vacancies,
+                isActive = opportunityModel.IsActive,
+                category = opportunityModel.Category,
+                description = opportunityModel.Description,
+                location = opportunityModel.Location,
+                address = opportunityModel.Address,
+                
+            };
+        }
+    }
+}
