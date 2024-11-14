76d2fa4238077bdb8dce22e3fdd981dc1883bcc8
JOAQUIM FONTES MATOS <joaquimatos50@gmail.com>
2024-11-14 20:51:46 +0000

    simplifiquei o componente

--- frontend/lib/Components/on_the_rise_opportunity_card.dart
+++ frontend/lib/Components/on_the_rise_opportunity_card.dart
@@ -33,165 +33,89 @@ class OnTheRiseOpportunityCard extends StatelessWidget {
   }
 
   List<Widget> _buildColumnContent() {
-    return [
-      // Image section
-      Stack(
-        children: [
-          Image.network(
-            'https://picsum.photos/200',
-            fit: BoxFit.cover,
-            height: 100,
-            width: double.infinity,
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Container(
-              padding: const EdgeInsets.all(4.0),
-              color: Colors.black54,
-              child: Text(
-                opportunity.category.toString().split('.').last,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-
-      SizedBox(width: 16, height: 16), // Spacing between image and text
-
-      // Description section
-
-      Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Text(
-                    opportunity.name,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SizedBox(width: 8),
-                TextButton(onPressed: null, child: Text("Detalhes")),
-              ],
-            ),
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("${opportunity.price.toString()}€/Pessoa"),
-                SizedBox(width: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      opportunity.location.toString().split('.').last,
-                    ),
-                    SizedBox(width: 8),
-                    IconButton.filled(onPressed: null, icon: Icon(Icons.star)),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    ];
+    return [_buildImageSection(), _buildDetailsSection()];
   }
 
   List<Widget> _buildRowContent() {
     return [
-      // Image section
       Expanded(
         flex: 1,
-        child: Stack(
-          children: [
-            Image.network(
-              'https://picsum.photos/200',
-              fit: BoxFit.cover,
-              height: 100,
-              width: double.infinity,
+        child: _buildImageSection(),
+      ),
+      Expanded(flex: 1, child: _buildDetailsSection()),
+    ];
+  }
+
+  Widget _buildImageSection() {
+    return Stack(
+      children: [
+        Image.network(
+          'https://picsum.photos/200',
+          fit: BoxFit.cover,
+          height: 100,
+          width: double.infinity,
+        ),
+        Positioned(
+          top: 0,
+          left: 0,
+          child: Container(
+            padding: const EdgeInsets.all(4.0),
+            color: Colors.black54,
+            child: Text(
+              opportunity.category.toString().split('.').last,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
             ),
-            Positioned(
-              top: 0,
-              left: 0,
-              child: Container(
-                padding: const EdgeInsets.all(4.0),
-                color: Colors.black54,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDetailsSection() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
                 child: Text(
-                  opportunity.category.toString().split('.').last,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
+                  opportunity.name,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
-
-      SizedBox(width: 16, height: 16), // Spacing between image and text
-
-      // Description section
-      Expanded(
-        flex: 1,
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
+              SizedBox(width: 8),
+              TextButton(onPressed: null, child: Text("Detalhes")),
+            ],
+          ),
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              Text("${opportunity.price.toString()}€/Pessoa"),
+              SizedBox(width: 8),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Expanded(
-                    child: Text(
-                      opportunity.name,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
+                  Text(
+                    opportunity.location.toString().split('.').last,
                   ),
                   SizedBox(width: 8),
-                  TextButton(onPressed: null, child: Text("Detalhes")),
-                ],
-              ),
-              SizedBox(height: 8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("${opportunity.price.toString()}€/Pessoa"),
-                  SizedBox(width: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        opportunity.location.toString().split('.').last,
-                      ),
-                      SizedBox(width: 8),
-                      IconButton.filled(
-                          onPressed: null, icon: Icon(Icons.star)),
-                    ],
-                  ),
+                  IconButton.filled(onPressed: null, icon: Icon(Icons.star)),
                 ],
               ),
             ],
           ),
-        ),
+        ],
       ),
-    ];
+    );
   }
 }
