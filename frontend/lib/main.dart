e49cdac27b7f63c30a8c5862c4c3673322d437f6
fbarbosa2 <f_barbosa@outlook.pt>
2024-11-18 15:32:50 +0000

    navRoute preparations

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -13,11 +13,23 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // Removes the debug banner
-      title: 'Flutter App',
+      title: 'Portal de Oportunidades',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: OpportunityDetailsScreen(), // No need to use `.new`
+      home: OpportunityDetailsScreen(),
+      /*
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/search': (context) => const SearchPage(),
+        '/profile': (context) => const ProfilePage(),
+        '/favorites': (context) => const FavoritesPage(),
+        '/create-opportunity': (context) => const CreateOpportunityPage(),
+        '/your-opportunities': (context) => const YourOpportunitiesPage(),
+        '/your-reservations': (context) => const YourReservationsPage(),
+      },
+      */
     );
   }
 }
