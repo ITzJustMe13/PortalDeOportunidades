77a090e0774f5020df05b6605eb450e03a369c99
JOAQUIM FONTES MATOS <joaquimatos50@gmail.com>
2024-12-01 19:03:19 +0000

    adicionei um botao de login por google

--- frontend/lib/Services/auth_Service.dart
+++ frontend/lib/Services/auth_Service.dart
@@ -4,31 +4,36 @@ import 'package:http/http.dart' as http;
 
 // Instância do GoogleSignIn para Web
 final GoogleSignIn _googleSignIn = GoogleSignIn(
-  clientId: '348791959445-mo2sbugjbvmebd4ag0usmlupff0gv35l.apps.googleusercontent.com',  // Substitua pela sua Client ID da Web
+  clientId:
+      '348791959445-mo2sbugjbvmebd4ag0usmlupff0gv35l.apps.googleusercontent.com', // Substitua pela sua Client ID da Web
 );
 
 class AuthService {
-  Future<void> signInWithGoogle() async {
+  Future<bool> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       if (googleUser == null) {
         // Login cancelado pelo usuário
-        return;
+        return false;
       }
 
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
 
       final String? idToken = googleAuth.idToken;
 
       if (idToken != null) {
-        await sendIdTokenToBackend(idToken);
+        bool isLoggedIn = await sendIdTokenToBackend(idToken);
+        return isLoggedIn;
       }
+      return false;
     } catch (error) {
       print("Erro ao fazer login com Google: $error");
+      return false;
     }
   }
 
-  Future<void> sendIdTokenToBackend(String idToken) async {
+  Future<bool> sendIdTokenToBackend(String idToken) async {
     final response = await http.post(
       Uri.parse('https://sua-api.com/google-login'),
       headers: {'Content-Type': 'application/json'},
@@ -37,8 +42,10 @@ class AuthService {
 
     if (response.statusCode == 200) {
       print("Login bem-sucedido!");
+      return true;
     } else {
       print("Erro no login: ${response.body}");
+      return false;
     }
   }
 }
